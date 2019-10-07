796912d1d72a903023ad067af193b5789d910df1
Erdika Rhamadan K <erdikar.kusumah@mail.unpas.ac.id>
2019-10-07 10:56:00 +0700

    menambahkan atribut controller, method, dan params

--- app/core/App.php
+++ app/core/App.php
@@ -6,26 +6,33 @@ class App {
 	protected $params = [];
 
 	public function __construct() {
+
 		$url = $this->parseURL();
 
-		if(file_exists('../app/controllers/' . $url[0] . '.php')) {
+		//controller
+		if ( file_exists('../app/controllers/' . $url[0] . '.php')) {
 			$this->controller = $url[0];
 			unset($url[0]);
-			var_dump($url);
 		}
 
-		require_once '../app/controller/' . $this->controller . '.php';
+		require_once '../app/controllers/' . $this->controller . '.php';
 		$this->controller = new $this->controller;
 
 		//method
 		if (isset($url[1])) {
-			if (method_exists(controller, $url[1]){
+			if (method_exists($this->controller, $url[1])){
 				$this->method = $url[1];
 				unset($url[1]);
 			}
 		}
 
 		//parameter
+		if ( !empty($url)) {
+			$this->params = array_values($url);
+		}
+
+		//jalankan controller dan method, serta kirimkan params jika ada
+		call_user_func_array([$this->controller, $this->method], $this->params);
 		
 	}
 
