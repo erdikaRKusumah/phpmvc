ecd83dc716977ca187e6661c83d710adbd1987a5
Erdika Rhamadan K <erdikar.kusumah@mail.unpas.ac.id>
2019-10-07 16:42:39 +0700

    membuat halaman about dan home dengan bootstrap

--- app/view/about/index.php
+++ app/view/about/index.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Halaman About</title>
-</head>
-<body>
+<div class="container">
 	<h1>About me</h1>
+	<img src="<?= BASEURL; ?>/img/erd.jpg" alt="Erdika R Kusumah" width="200" class="rounded-circle shadow">
 	<p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
 
-</body>
-</html>
+</div>
